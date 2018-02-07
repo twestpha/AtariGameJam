b9a26571479517116aa685d9169808bc4a2be29b
Chris Rice <chrisrice93@gmail.com>
2018-02-06 18:17:52 -0700

    Added basic circle-in attack for boss, has some problems but mostly works

---
+++ Assets/scripts/boss_attacks/CircleInAttack.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.Assertions.Comparers;
+
+public class CircleInAttack : MonoBehaviour {
+
+	public GameObject bulletPrefab;
+	public float startingRadius;
+	public int numberOfBullets;
+	public float warningTimeSeconds;
+
+	private Timer warningTimer;
+
+	private GameObject[] bullets;
+	private bool bulletsActivated = false;
+	
+	// Use this for initialization
+	void Start () {
+		Vector3 centerTarget = GameObject.FindWithTag("Player").transform.position;
+
+		bullets = new GameObject[numberOfBullets];
+		for (int i = 0; i < numberOfBullets; i++) {
+			float angleInRad = (2.0f * Mathf.PI * i) / numberOfBullets;
+			float x = Mathf.Cos(angleInRad) * startingRadius + centerTarget.x;
+			float y = Mathf.Sin(angleInRad) * startingRadius + centerTarget.y;
+			GameObject bullet = Instantiate(bulletPrefab, new Vector3(x, 0, y), Quaternion.identity); 
+			bullet.SendMessage("SetTarget", centerTarget);
+			bullets[i] = bullet;
+		}
+		
+		warningTimer = new Timer(warningTimeSeconds);
+		warningTimer.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!bulletsActivated && warningTimer.Finished()) {
+			Debug.Log("Finished warning, time to die!");
+			for (int i = 0; i < numberOfBullets; i++) {
+				// Some bullets might not be there because the player can run into them
+				bullets[i].SendMessage("Activate");
+			}
+
+			bulletsActivated = true;
+		}
+	}
+}
---
+++ Assets/scripts/components/StaticTargetMissleComponent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticTargetMissleComponent : MonoBehaviour {
+    public float speed;
+    
+    private Vector3 target;
+    private bool isActive;
+    private float minDestroyDistance = 0.05f;
+
+    public GameObject collisionEffectPrefab;
+    
+    public void SetTarget(Vector3 target) {
+        this.target = target;
+    }
+
+    public void Activate() {
+        isActive = true;
+    }
+
+    void Update() {
+        if (isActive) {
+            Vector3 playerVector = target - transform.position;
+            playerVector.y = 0.0f;
+            playerVector.Normalize();
+            Vector3 velocity = playerVector * speed;
+            transform.position += velocity * Time.deltaTime;
+            
+            if((transform.position - target).magnitude < minDestroyDistance){
+                if(collisionEffectPrefab){
+                    GameObject collfx = Object.Instantiate(collisionEffectPrefab);
+                    collfx.transform.position = transform.position;
+                }
+                Destroy(gameObject);
+            }
+        }
+    }
+}
