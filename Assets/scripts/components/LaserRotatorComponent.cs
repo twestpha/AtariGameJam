d5963ea88ecc45da2093df21311abf6fb11cbda1
Trevor Westphal <trevorawestphal@gmail.com>
2018-02-10 15:05:06 -0800

    dumb ass

--- Assets/scripts/components/LaserRotatorComponent.cs
+++ Assets/scripts/components/LaserRotatorComponent.cs
@@ -6,7 +6,7 @@ public class LaserRotatorComponent : MonoBehaviour {
 
     public float spinUpTime;
     public float spinDownTime;
-    
+
     private Timer spinUpTimer;
     private Timer spinDownTimer;
 
@@ -26,11 +26,11 @@ public class LaserRotatorComponent : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if(up){
-            a.transform.localScale = new Vector3(1.0f, 50.0f, spinUpTimer.Parameterized());
-            b.transform.localScale = new Vector3(1.0f, 50.0f, spinUpTimer.Parameterized());
+            a.transform.localScale = new Vector3(1.0f, 50.0f * spinUpTimer.Parameterized(), spinUpTimer.Parameterized());
+            b.transform.localScale = new Vector3(1.0f, 50.0f * spinUpTimer.Parameterized(), spinUpTimer.Parameterized());
         } else {
-            a.transform.localScale = new Vector3(1.0f, 50.0f, 1.0f - spinDownTimer.Parameterized());
-            b.transform.localScale = new Vector3(1.0f, 50.0f, 1.0f - spinDownTimer.Parameterized());
+            a.transform.localScale = new Vector3(1.0f, 50.0f * 1.0f - spinDownTimer.Parameterized(), 1.0f - spinDownTimer.Parameterized());
+            b.transform.localScale = new Vector3(1.0f, 50.0f * 1.0f - spinDownTimer.Parameterized(), 1.0f - spinDownTimer.Parameterized());
         }
 
         if(spinUpTimer.Finished() && up){
