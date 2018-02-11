d5c0afee0254368449cbe0177de3ce97a64fc67f
Chris Rice <chrisrice93@gmail.com>
2018-02-10 21:43:03 -0700

    saw blade wave attack

---
+++ Assets/scripts/components/DirectionalProjectileComponent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DirectionalProjectileComponent : MonoBehaviour {
+
+	public float speed = 0.0f;
+	public float destroyDistance = 100.0f;
+	public Vector3 direction;
+
+	public float damage;
+
+	private Vector3 startPosition;
+
+	void Start(){
+		startPosition = transform.position;
+	}
+
+	void Update(){
+		transform.position += direction * speed * Time.deltaTime;
+
+		if((transform.position - startPosition).magnitude > destroyDistance){
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/scripts/components/WaveAttackComponent.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveAttackComponent : MonoBehaviour {
+
+	public GameObject projectilePrefab;
+	public int numberOfWaves;
+	public float secondsBetweenWaves;
+	
+	private Timer spawnTimer;
+	private GameObject enemy;
+
+	private float[] possibleZs;
+	private int wavesCompleted;
+	
+	// Use this for initialization
+	void Start () {
+		enemy = GameObject.FindWithTag("Enemy");
+
+		possibleZs = new float[] {14, 7, 0, -7, -14};
+		
+		spawnTimer = new Timer(secondsBetweenWaves);
+		spawnTimer.Start();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (spawnTimer.Finished()) {
+			wavesCompleted += 1;
+			spawnThings();
+			spawnTimer = new Timer(secondsBetweenWaves);
+			spawnTimer.Start();
+		}
+
+		if (wavesCompleted == numberOfWaves) {
+			Destroy(gameObject);
+		}
+			
+	}
+
+	void spawnThings() {
+		int skipIndex = Random.Range(0, 5);
+		for (int i = 0; i < possibleZs.Length; i++) {
+			if (i != skipIndex) {
+				GameObject projectile = Instantiate(
+					projectilePrefab, 
+					new Vector3(enemy.transform.position.x, 0, possibleZs[i]), 
+					projectilePrefab.transform.rotation);
+				projectile.GetComponent<DamagingComponent>().creator = enemy;
+
+			}
+		}
+	}
+}
